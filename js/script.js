b4baa9edf3c9870abbec39d0ba777b6d0df2cd4b
Shumaila Kayani <shumailakayani059@gmail.com>
2021-06-09 12:18:01 +0200

    About page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let url = "https://flowerpowerlcb.com/wp-json/wp/v2/posts?_embed&per_page=3";
+
 /* Navigration Bar */
 
 const navSlide = () => {
@@ -33,3 +35,56 @@ const navSlide = () => {
 }
 
 navSlide();
+
+/* Loader */
+
+window.addEventListener("load", function(){
+    const loader = document.querySelector(".loader");
+    
+    loader.className += " hidden";
+    
+    })
+
+
+
+
+
+/* bringing the last 3 posts */
+
+const blogPosts = document.querySelector(".blogPosts");
+
+fetch(url)
+.then (response => response.json())
+.then (data =>{
+    postList(data);
+    console.log(data);
+})
+
+.catch((error)=>{
+    console.error('Error', error);
+})
+
+function postList(posts){
+    let pList ="";
+
+    for (let item in posts){
+        console.log(posts[item]);
+
+        pList += `
+<div class="postDiv">
+<a href="blogpost.html?id=${posts[item].id}">
+<h3 class="blogPostHeading">${posts[item].title.rendered}</h3>
+<div class="imgdiv"><img src="${posts[item]._embedded["wp:featuredmedia"][0].source_url}"></div>
+</a>
+</div>
+`
+
+    }
+
+    blogPosts.innerHTML = pList;
+}
+
+
+
+
+
